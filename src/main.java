54445afef34cfe16e69a77df3561cb2c0d01437f
russellsean <sean.russell@nwciowa.edu>
2016-01-07 13:34:07 -0600

    Shit went down
    Like promised. There is no more procedural code. Every button, and every
    menu item is created with a method. This just cut off almost 1000 lines
    of code, and saved about 100 variables. Dynamic programming is awesome.
    This code looks freakin sexy, and the program feels way more smooth to
    interact with.

--- src/main.java
+++ src/main.java
@@ -10,7 +10,7 @@ import java.util.*;
  * TJ.Gudeman and Sean.Russell logic checked the UI, and how it interacts with the user
  * Mark.Peterson did the 'back-end' programming. Writing/Reading all files.
  * 
- *@purpose This Program functions as a password manager. The user will enter their 
+ * @purpose This Program functions as a password manager. The user will enter their 
  *			information to create an account and the program  will store their 
  *			information, so the user need to remember one master password.
  */
